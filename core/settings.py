064f7a92f3c5a65089a4bab0ed05bd813e8dc6fd
Marcelo Campos <marc3lo99moreira@gmail.com>
2026-04-10 01:37:12 +0100

    Correção de erro no WhiteNoise

--- core/settings.py
+++ core/settings.py
@@ -198,7 +198,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
